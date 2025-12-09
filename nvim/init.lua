7a76c8112ad3c43afa4e9be44204c6a21147aa25
Karl Bartel <karl.bartel@clabs.co>
2025-12-09 16:31:49 +0100

    vim: add buffer completion source

--- nvim/init.lua
+++ nvim/init.lua
@@ -607,7 +607,7 @@ require("lazy").setup({
       },
 
       sources = {
-        default = { "lsp", "path", "snippets", "lazydev" },
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
         providers = {
           lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
         },
