7056edc863c1eda80f0625c2cbdabce2ab09b6cb
Karl Bartel <karl.bartel@clabs.co>
2025-12-09 16:18:30 +0100

    install: no vim and tmux plugin installation

--- install.sh
+++ install.sh
@@ -31,11 +31,3 @@ cp -rn ~/.myconfig/templates/.??* ~/
 # mc: only copy file, since mc constantly changes some unimportant settings
 mkdir -p ~/.config/mc
 cp -f ~/.myconfig/.mc/ini ~/.config/mc/ini
-
-# install tmux plugins
-[ -e ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
-
-# install vim plugins 
-[ -e ~/.vim/pack/minpac/opt/minpac ] || git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
-vim +PackUpdate +qall  # "qall" quits vim after installing
