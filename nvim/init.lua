f22d95256901f8a974e2ca7ab9dbefc4878824ba
Karl Bartel <karl.bartel@clabs.co>
2025-12-17 08:46:20 +0100

    vim: Disable blink.cmp's Ctrl-P/Ctrl-N so Vim's native buffer completion works

--- nvim/init.lua
+++ nvim/init.lua
@@ -601,7 +601,7 @@ require("lazy").setup({
       },
 
       sources = {
-        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        default = { "lsp", "path", "snippets", "lazydev" },
         providers = {
           lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
         },
