27b98c9c9bba2f65e4485a52f16cd13ac30dd79e
Karl Bartel <karl.bartel@clabs.co>
2026-04-29 15:31:48 +0200

    vim: hoist telescope.builtin require in LspAttach

--- nvim/init.lua
+++ nvim/init.lua
@@ -246,15 +246,16 @@ require("lazy").setup({
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
           end
 
+          local tb = require("telescope.builtin")
           map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
           map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-          map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-          map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-          map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("grr", tb.lsp_references, "[G]oto [R]eferences")
+          map("gri", tb.lsp_implementations, "[G]oto [I]mplementation")
+          map("grd", tb.lsp_definitions, "[G]oto [D]efinition")
           map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-          map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
-          map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-          map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+          map("gO", tb.lsp_document_symbols, "Open Document Symbols")
+          map("gW", tb.lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+          map("grt", tb.lsp_type_definitions, "[G]oto [T]ype Definition")
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           -- Inlay hints displace code, so toggle rather than enable by default
