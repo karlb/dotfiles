d77c45035aba41a73ff8ea58450cdd7fa61d7aa6
Karl Bartel <karl.bartel@clabs.co>
2026-04-29 13:50:27 +0200

    vim: merge duplicate FocusGained autocmds

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,15 +70,19 @@ vim.o.updatetime = 250
 -- [[ Basic Autocommands ]]
 
 -- Reload files changed outside of Neovim
-vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
+vim.g.vim_focused = true
+vim.api.nvim_create_autocmd("FocusGained", {
+  callback = function()
+    vim.g.vim_focused = true
+    vim.cmd("checktime")
+  end,
+})
+vim.api.nvim_create_autocmd("FocusLost", { callback = function() vim.g.vim_focused = false end })
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   callback = function()
     if vim.g.vim_focused then vim.cmd("checktime") end
   end,
 })
-vim.g.vim_focused = true
-vim.api.nvim_create_autocmd("FocusGained", { callback = function() vim.g.vim_focused = true end })
-vim.api.nvim_create_autocmd("FocusLost", { callback = function() vim.g.vim_focused = false end })
 
 
 -- Highlight when yanking (copying) text. Try it with `yap` in normal mode
