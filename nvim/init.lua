58cf751539e848e6438075e408af14f1cdaab2de
Karl Bartel <karl.bartel@clabs.co>
2026-04-29 13:16:16 +0200

    vim: drop ruby treesitter regex/indent overrides

--- nvim/init.lua
+++ nvim/init.lua
@@ -444,14 +444,8 @@ require("lazy").setup({
     main = "nvim-treesitter.configs", -- Sets main module to use for opts
     opts = {
       auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = true, disable = { "ruby" } },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
   },
   {
