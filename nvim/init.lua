a3675d2f47093f5eb269ef19d0113b4572141810
Karl Bartel <karl.bartel@clabs.co>
2026-04-29 13:51:09 +0200

    vim: drop kickstart prefix from highlight-yank augroup

--- nvim/init.lua
+++ nvim/init.lua
@@ -85,7 +85,7 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 -- Highlight when yanking (copying) text. Try it with `yap` in normal mode
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
