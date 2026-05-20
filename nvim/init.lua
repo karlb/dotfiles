4f993134564d3928436aff6dbf7b849e8fd90143
Karl Bartel <karl.bartel@clabs.co>
2026-05-20 17:09:05 +0200

    vim: disable vim-illuminate in markdown files

--- nvim/init.lua
+++ nvim/init.lua
@@ -393,7 +393,12 @@ require("lazy").setup({
   },
 
   -- Highlight other uses of the word under the cursor (LSP → treesitter → regex)
-  "RRethy/vim-illuminate",
+  {
+    "RRethy/vim-illuminate",
+    config = function()
+      require("illuminate").configure({ filetypes_denylist = { "markdown" } })
+    end,
+  },
 
   { -- Collection of various small independent plugins/modules
     "echasnovski/mini.nvim",
