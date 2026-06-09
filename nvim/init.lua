9ea130e848b3f75183ff6901fe786f1ed1db48b1
Karl Bartel <karl.bartel@clabs.co>
2026-06-09 12:34:02 +0200

    nvim: enable spellcheck globally

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,7 @@ vim.o.undofile = true  -- Save undo history
 vim.o.inccommand = "split"  -- Preview substitutions live, as you type!
 vim.o.cursorline = true  -- Show which line your cursor is on
 vim.o.scrolloff = 5  -- Minimal number of screen lines to keep above and below the cursor.
+vim.o.spell = true
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
